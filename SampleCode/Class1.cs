cef13e0819a08b4c14ce5ee91a674400660f2401
Bhaktikanchan532 <kanchanbhakti@gmail.com>
2022-09-29 23:03:53 +0530

    Relational

---
+++ SampleCode/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCode
+{
+    internal class Class1
+    {
+        static void Main(String[] args)
+        {
+            int a = 10, b = 9;
+            bool c = a > b;
+            Console.WriteLine(c);
+            bool d = a < b;
+            Console.WriteLine(d);
+            bool e = a >= b;
+            Console.WriteLine(e);
+            bool f = a <= b;
+            Console.WriteLine(f);
+            bool g = a == b;
+            Console.WriteLine(g);
+            bool h = a != b;
+            Console.WriteLine(h);
+        }
+    }
+}
--- SampleCode/Program.cs
+++ SampleCode/Program.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace SampleCode
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello");
-        }
-    }
-}
+//namespace SampleCode
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello");
+//        }
+//    }
+//}
